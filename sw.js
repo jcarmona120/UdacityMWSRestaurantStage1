ef3ea06e3c7145a56e58492b3d2a5a7c0b2e9c04
Jawann Carmona <carmona.jawann@gmail.com>
2018-06-12 22:18:51 -0400

    updated sw to cache all assets. added all urls to cache

--- sw.js
+++ sw.js
@@ -1,5 +1,36 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js').then(function() {
-        console.log('Service Worker Registered')
-    })
-}
+var CACHE_NAME = 'restaurantreviews-cache-v1';
+var urlsToCache = [
+    '/',
+    './index.html',
+    './restaurant.html',
+    './css/styles.css',
+    './js/dbhelper.js',
+    './js/main.js',
+    './js/restaurant_info.js',
+    '/data/restaurants.json'
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
